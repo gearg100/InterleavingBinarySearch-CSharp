7fe9e00f3d1fb4c344c24c80ab2c57c16a265277
Georgios Psaropoulos <gearg100@windowslive.com>
2019-05-11 17:24:09 +0200

    Add implementations

---
+++ Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Intrinsics.X86;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Runtime.CompilerServices;
+
+namespace BinarySearch
+{
+    static class Impl
+    {
+        public static int[] array;
+        public static int[] values;
+
+        public static void Setup(int N, int V)
+        {
+            array = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = i;
+            }
+            values = new int[V];
+            var rng = new Random();
+            for (int i = 0; i < V; i++)
+            {
+                values[i] = rng.Next(N);
+            }
+        }
+
+        public static void Sequential(int V, int _, int[] array, int[] values)
+        {
+            List<int> res = new List<int> { };
+            res.Capacity = V;
+            foreach (var val in values)
+            {
+                int low = 0, size = array.Length;
+
+                while (size > 1)
+                {
+                    int probe = low + size / 2;
+                    int v = array[probe];
+                    if (v < val) low = probe;
+                    size -= size / 2;
+                }
+
+                if (size == 1 && array[low] < val) low++;
+                res.Add(low);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static unsafe void Prefetch(int[] arr, int idx)
+        {
+            fixed (int* ptr = arr)
+            {
+                Sse.Prefetch0(ptr + idx);
+            }
+        }
+
+        public static void InterleavedTask(int V, int G, int[] array, int[] values)
+        {
+            List<int> res = new List<int> { };
+            res.Capacity = V;
+            var tasks = Enumerable.Range(0, G).Select(async (i) =>
+            {
+                while (i < V)
+                {
+                    var idx = i; i += G;
+                    var val = values[idx];
+                    int low = 0, size = array.Length;
+
+                    while (size > 1)
+                    {
+                        int probe = low + size / 2;
+                        Prefetch(array, probe);
+                        await Task.Yield();
+                        int v = array[probe];
+                        if (v < val) low = probe;
+                        size -= size / 2;
+                    }
+
+                    if (size == 1 && array[low] < val) low++;
+                    res.Add(low);
+                }
+            });
+            Task.WaitAll(tasks.ToArray());
+        } 
+
+        class Res<T>
+        {
+            public T Value;
+        }
+
+        static IEnumerable<ValueTuple> ManySearches(int G, int[] array, int[] values, List<int> res, int i)
+        {
+            while (i < values.Length)
+            {
+                var idx = i; i += G;
+                var val = values[idx];
+                int low = 0, size = array.Length;
+
+                while (size > 1)
+                {
+                    int probe = low + size / 2;
+                    Prefetch(array, probe);
+                    yield return new ValueTuple();
+                    int v = array[probe];
+                    if (v < val) low = probe;
+                    size -= size / 2;
+                }
+
+                if (size == 1 && array[low] < val) low++;
+                res.Add(low);
+            }
+        }
+
+
+        public static void InterleavedEnumerable(int V, int G, int[] array, int[] values)
+        {
+            List<int> res = new List<int> { };
+            res.Capacity = V;
+            var tasks = Enumerable.Range(0, G).Select((i) => ManySearches(G, array, values, res, i).GetEnumerator()).ToArray();
+            int remaining = G;
+            while (G > 0)
+            {
+                for(int i = 0; i < tasks.Length; i++)
+                {
+                    if (tasks[i] == null) continue;
+                    if (!tasks[i].MoveNext())
+                    {
+                        tasks[i] = null;
+                        G--;
+                    }
+                }
+            }
+        }
+    }
+
+
+    [CoreJob]
+    [RPlotExporter, RankColumn]
+    public class Bench
+    {
+        [Params(1 * 1024 * 1024, 1 * 1024 * 1024 * 1024)]
+        public int N;
+
+        [Params(10000)]
+        public int V;
+
+        [Params(10)]
+        public int G;
+
+        [GlobalSetup]
+        public void Setup() => Impl.Setup(N, V);
+
+        [Benchmark]
+        public void Sequential() => Impl.Sequential(V, G, Impl.array, Impl.values);
+
+        [Benchmark]
+        public void InterleavedTask() => Impl.InterleavedTask(V, G, Impl.array, Impl.values);
+
+        [Benchmark]
+        public void InterleavedEnumerable() => Impl.InterleavedEnumerable(V, G, Impl.array, Impl.values);
+
+    }
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Bench>();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# InterleavingBinarySearch-CSharp
+# Interleaving Binary Search in CSharp (.Net Core 3.0)
+Two implementations of interleaved execution for binary search in CSharp using:
+- async/await, and
+- yield.
+
+## Benchmark results
+N: array size, V: number of values to look for, G: group size
+
+``` ini
+
+BenchmarkDotNet=v0.11.5, OS=Windows 10.0.18362
+Intel Core i7-7500U CPU 2.70GHz (Kaby Lake), 1 CPU, 4 logical and 2 physical cores
+.NET Core SDK=3.0.100-preview5-011568
+  [Host] : .NET Core 3.0.0-preview5-27626-15 (CoreCLR 4.6.27622.75, CoreFX 4.700.19.22408), 64bit RyuJIT
+  Core   : .NET Core 3.0.0-preview5-27626-15 (CoreCLR 4.6.27622.75, CoreFX 4.700.19.22408), 64bit RyuJIT
+
+Job=Core  Runtime=Core  
+
+```
+|                Method |          N |     V |  G |      Mean |     Error |    StdDev |    Median | Rank |
+|---------------------- |----------- |------ |--- |----------:|----------:|----------:|----------:|-----:|
+|            **Sequential** |    **1048576** | **10000** | **10** |  **2.520 ms** | **0.0500 ms** | **0.1096 ms** |  **2.453 ms** |    **1** |
+|       InterleavedTask |    1048576 | 10000 | 10 | 21.465 ms | 0.3984 ms | 0.4092 ms | 21.404 ms |    5 |
+| InterleavedEnumerable |    1048576 | 10000 | 10 |  2.720 ms | 0.0276 ms | 0.0231 ms |  2.720 ms |    2 |
+|            **Sequential** | **1073741824** | **10000** | **10** | **17.501 ms** | **0.3824 ms** | **1.1217 ms** | **17.141 ms** |    **4** |
+|       InterleavedTask | 1073741824 | 10000 | 10 | 34.454 ms | 0.6842 ms | 0.8653 ms | 34.425 ms |    6 |
+| InterleavedEnumerable | 1073741824 | 10000 | 10 |  6.558 ms | 0.0359 ms | 0.0300 ms |  6.550 ms |    3 |
